20f1395836c7a2a75a0af31fb353f398391d2831
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-02-05 00:43:41 +0200

    Add task2 Cylinder Volume

---
+++ src/Task3/NumberPower.java
@@ -0,0 +1,22 @@
+package Task3;
+
+import java.util.Scanner;
+
+public class NumberPower {
+    public static int numPower(int a, int b) {
+        if (b == 0) {
+            return 1;
+        } else {
+            return (a * numPower(a, b - 1));
+        }
+    }
+    public static void main (String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a =>");
+        int a = scanner.nextInt();
+        System.out.println("Enter b =>");
+        int b = scanner.nextInt();
+
+        System.out.println(a+"^"+b+"="+numPower(a,b));
+    }
+}
