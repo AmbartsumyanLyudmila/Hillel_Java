284d611a55a6b75ec29dbdd54bf3c546e4283fa4
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-02-04 23:41:58 +0200

    "Task1 was added"

---
+++ src/Task1/SquareNumber.java
@@ -0,0 +1,28 @@
+package Task1;
+
+public class SquareNumber {
+    public static void number(int number) {
+        System.out.println(number*number);
+    }
+    public static void main(String[] args){
+        System.out.print("The square of 1 = ");
+        number(1);
+        System.out.print("The square of 2 = ");
+        number(2);
+        System.out.print("The square of 3 = ");
+        number(3);
+        System.out.print("The square of 4 = ");
+        number(4);
+        System.out.print("The square of 5 = ");
+        number(5);
+        System.out.print("The square of 6 = ");
+        number(6);
+        System.out.print("The square of 7 = ");
+        number(7);
+        System.out.print("The square of 8 = ");
+        number(8);
+        System.out.print("The square of 9 = ");
+        number(9);
+
+    }
+}
