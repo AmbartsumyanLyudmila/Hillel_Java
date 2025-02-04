ed706bfa2ede8a4b77b5be689408ad9e5a86b669
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-02-05 00:19:35 +0200

    Add task2 Cylinder Volume

---
+++ src/Task2/CylinderVolume.java
@@ -0,0 +1,17 @@
+package Task2;
+
+public class CylinderVolume {
+    public static double cylinderVolume(double radius, double height) {
+        return Math.PI * radius * radius * height;
+    }
+
+    public static void main(String[] args) {
+        double radius = 2;
+        double height = 10;
+
+        double volume = cylinderVolume(radius, height);
+        System.out.println("radius = "+ radius);
+        System.out.println("height = "+ height);
+        System.out.println("V = "+ volume);
+    }
+}
