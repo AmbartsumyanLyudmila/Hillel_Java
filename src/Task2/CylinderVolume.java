1f1aec4937a62071b328b67c79c4bf6fdec95ed0
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-02-05 00:48:22 +0200

    Update task2

--- src/Task2/CylinderVolume.java
+++ src/Task2/CylinderVolume.java
@@ -6,8 +6,8 @@ public class CylinderVolume {
     }
 
     public static void main(String[] args) {
-        double radius = 3;
-        double height = 9;
+        double radius = 10;
+        double height = 5;
 
         double volume = cylinderVolume(radius, height);
         System.out.println("radius = "+ radius);
