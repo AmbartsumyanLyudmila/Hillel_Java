60df05c93ae0c3cf0e58d5bbea54336f1ec163db
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-02-14 01:45:30 +0200

    Add new Task 6_3 Reverse_Array

--- src/Task6_3/ReverseArray.java
+++ src/Task6_3/ReverseArray.java
@@ -14,7 +14,7 @@ public class ReverseArray {
     }
 
     public static void main(String[] args) {
-        int[] numbers = {0, 11, 22, 33, 44, 55, 66, 77, 88, 99, 111, 222, 333, 444, 555, 666, 777, 888, 999};
+        int[] numbers = {0, 11, 22, 33, 44, 55, 66, 77, 88, 99, 111, 222, 333, 444, 555, 666, 777, 888, 999, 1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999};
         System.out.print("Array : ");
         getArray(numbers);
         System.out.println();
