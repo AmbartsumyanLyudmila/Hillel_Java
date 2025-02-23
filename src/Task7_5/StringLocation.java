c48546e7fa7638a23f5f53f02d6089c988dcb54a
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-02-23 23:39:21 +0200

    Add new Task 7_5 xyzThere

--- src/Task7_5/StringLocation.java
+++ src/Task7_5/StringLocation.java
@@ -12,7 +12,6 @@ public class StringLocation {
         return false;
     }
 
-
     public static void main(String[] args) {
         System.out.println(xyzThere("abcxyz"));
         System.out.println(xyzThere("abc.xyz"));
