4e964f5bf15d57348f155df27dbe1a4165ad2d51
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-03-23 22:51:05 +0200

    Add new Task10_8 TreeSetUnification

---
+++ src/Task10_8/TreeSetUnification.java
@@ -0,0 +1,16 @@
+package Task10_8;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetUnification {
+    public static void main(String[] args) {
+        TreeSet<Integer> treeSetA = new TreeSet<>(Set.of(1, 2, 3));
+        TreeSet<Integer> treeSetB = new TreeSet<>(Set.of(3, 4, 5));
+        TreeSet<Integer> result = new TreeSet<>();
+        result.addAll(treeSetA);
+        result.addAll(treeSetB);
+        System.out.println("Combined TreeSet: " + result);
+    }
+}
+
